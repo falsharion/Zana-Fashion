bba6ff1481350b0afcdb169cbbb4423d870f2374
Falomo Sharon simisola <92723282+falsharion@users.noreply.github.com>
2022-04-12 22:21:49 +0100

    added cool mode to the background and meta tags

---
+++ script.js
@@ -0,0 +1,6 @@
+// to toggle the background
+let button = document.querySelector(".theme-toggler");
+let calculator = document.querySelector(".overall");
+function darkTheme() {
+    calculator.classList.toggle("dark")
+};
